802bc9facbc941918ffb0425fcf14008d5fe88fd
84176052+sounddrill31 <84176052+sounddrill31@users.noreply.github.com>
2025-01-04 15:39:50 +0530

    Solve initial bit manipulation qn

--- p3-bit-manipulation/main.c
+++ p3-bit-manipulation/main.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+static int n = 16;
 /*
 Note:
   This is an interactive program in which
@@ -12,34 +12,66 @@ Run the below command to compile and run this program.
 void toggle_every_kth_room(int *lights, int k)
 {
   // Code here
+  int mask = 0;
+  for(int i = (k-1); i<16;(i+=k))
+  {
+    mask = 1<<i;
+  }
+  *lights ^= mask; //XOR Equal to
 }
 
 void toggle_even_rooms(int *lights)
 {
   // Use the above function to toggle the even rooms
   // toggle_every_kth_room(...);
+  toggle_every_kth_room(lights, 2); //2 increment = even
 }
 
 int get_mask()
 {
+  int i, mask = 0;
   // Read the input till -1 and create the mask
+  while(1) //infinite loop
+  {
+    scanf("%d", &i);
+    if(i==-1)
+      break;
+    mask |= 1<<(i-1); //Or Equal to
+    return mask;
+  }
+
 }
 
 void turn_on_lights(int *lights)
 {
   // apply mask to lights to turn on the particular lights
   int mask = get_mask();
+  //To turn the bits - "OR =" operation
+  // To reset, "AND =" with Negation
+  *lights |= mask;
+
 }
 
 void turn_off_lights(int *lights)
 {
   // apply mask to lights to turn off the particular lights
   int mask = get_mask();
+  *lights &= ~mask;
 }
 
 int count_lights(int lights)
 {
+  int c = 0;
   // Code here
+  for(int i = 0; i<16;i++)
+  {
+    if((lights >> i) & i) //left shift, get i-th bit to lsb
+    {
+      c++;
+    }
+    return c;
+  }
+
 }
 
 void print_bin(int a)
